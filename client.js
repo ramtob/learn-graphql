f56c306972ac0e7c7fd9f6d8c9f23ee3361e8fd7
ramtob <ramtob@gmail.com>
2019-05-21 11:29:33 +0300

    Trying to send GraphQL request from client...

---
+++ client.js
@@ -0,0 +1,14 @@
+function do_request() {
+    fetch('http://localhost:4000/graphql', {
+        method: 'POST',
+        mode: 'no-cors',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({query: "{ hello }"})
+    })
+        .then(r => r.json())
+        .then(data => console.log('data retured:', data))
+        .catch((err) => console.warn(err))
+}
---
+++ client1.js
@@ -0,0 +1,12 @@
+module.exports = function do_request() {
+    fetch('http://localhost:4000/graphql', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({query: "{ hello }"})
+    })
+        .then(r => r.json())
+        .then(data => console.log('data retured:', data));
+};
